aede7bdcd74e6a1529fde61823854a23fe67bcf4
pedrobrum <pedrobrum@dcc.ufmg.br>
2021-06-02 12:00:09 -0300

    Added canonical unit metrics file

--- src/item/pricing/utils.py
+++ src/item/pricing/utils.py
@@ -1,6 +1,10 @@
 # imports
 
 import collections
+import json
+from nlp.preprocessing_portuguese import remove_accents
+from nlp.preprocessing import PreprocessingText
+from gensim.parsing.preprocessing import strip_multiple_whitespaces
 
 
 def add_first_token_column(data):
@@ -59,6 +63,71 @@ def get_price(item_dict):
     return item_dict['preço']
 
 
+def isfloat(value):
+    value_ = value.replace(',','.')
+    try:
+        float(value_)
+        return True
+    except ValueError:
+        return False
+
+def remove_prefix(text, prefix):
+    return text[text.startswith(prefix) and len(prefix):]
+
+def remove_special_characters(text):
+    lista = '-#@%?º°ª:/;~^`[{]}\\|!$"\'&*()=+><\t\r\n…'
+    result = text
+    for i in range(0, len(lista)):
+        result = result.replace(lista[i], ' ')
+    return result
+
+def remove_dots(text):
+    lista = '.'
+    result = text
+    for i in range(0, len(lista)):
+        result = result.replace(lista[i], '')
+    return result
+
+def lemmatize_unit_metric(text, canonical_unit_metric):
+    if text in canonical_unit_metric:
+        return canonical_unit_metric[text]
+    return text
+
+def process_un_medida(un_medida, stopwords, preprocessing, canonical_unit_metric):
+
+    un_medida_process = str(un_medida).lower()
+
+    if len(un_medida_process) == 50:
+        un_medida_process = un_medida_process.lstrip('0')
+    un_medida_process = remove_prefix(un_medida_process, "1 ")
+    un_medida_process = remove_accents(un_medida_process)
+
+    un_medida_process = remove_special_characters(un_medida_process)
+    un_medida_process = remove_dots(un_medida_process)
+
+    un_medida_process = ' '+un_medida_process+' '
+    un_medida_process = re.sub(r' (\d+)([a-z]+) ', r' \1 \2 ', un_medida_process, flags=re.I)
+    un_medida_process = un_medida_process.strip()
+
+    items = un_medida_process.split(' ')
+    new_items = []
+    for item in items:
+        if item not in stopwords:
+            new_items.append(item)
+    items = new_items
+
+    items = preprocessing.lemmatization_document(items)
+    new_items = []
+    for item in items:
+        new_items.append(lemmatize_unit_metric(item, canonical_unit_metric))
+
+    un_medida_process = ' '.join(new_items)
+
+    un_medida_process = strip_multiple_whitespaces(un_medida_process)
+
+    return un_medida_process
+
+
 def group_dsc_unidade_medida(items_df):
     '''
         Group some of the unit metrics ('dsc_unidade_medida') in the dataframe
@@ -66,33 +135,19 @@ def group_dsc_unidade_medida(items_df):
         'cx' -> 'caixa', ['unitario', 'unid', 'und'] -> 'unidade'.
     '''
 
-    unidade = ['un', 'unid', 'und', 'uni', 'unidad', 'unid.', 'unitario', 'unidade.',
-                'un.', 'uni.', 'und.', 'unida', 'unita', 'und,', 'un1', 'unid..', 'um',
-                '0000000000000000000000000000000000000000000unidade', 'u']
-    unidades = ['unidade(s)', 'unidade_s', 'unidade _s', 'unids']
-    hora = ['hr', 'hrs', 'horas']
-    jogo = ['jg', 'jgs']
-    peca = ['pe', 'pc', 'pc.']
-    saco = ['sc', 'sacos']
-    caixa = ['cx', 'cx.', '000000000000000000000000000000000000000000000caixa', 'cxa']
-    comprimido = ['cp', 'comp', 'comprimido 1 unidade', 'comp.', 'cmp', 'compr',
-                  'comprimid', 'com', 'cpr', 'capsula', 'cap',
-                  '0000000000000000000000000000000000000000comprimido']
-    pacote = ['pct', 'pt', 'pcte', 'pacote.', 'pct.', 'pacot']
-    kg = ['kilo', 'quilo', 'quilograma', 'kilograma', 'kilos', 'quilos', 'quilogramas',
-          'kilogramas', 'quilo.', 'quilograma_s']
-    frasco = ['fr', 'frasco.', 'fras', 'frasco 1 unidade', 'frasco/ampola', 'frasc']
-    litro = ['litros', 'ltr', 'lit', 'litro.', 'li', 'l', 'lt', 'lts']
+    with open('../dados/palavras/unit_metric_canonical.json', 'r') as data:
+        unit_metric_canonical = json.load(data)
 
-    items_df.replace(unidade, value='unidade', inplace=True)
-    items_df.replace(unidades, value='unidades', inplace=True)
-    items_df.replace(hora, value='hora', inplace=True)
-    items_df.replace(jogo, value='jogo', inplace=True)
-    items_df.replace(peca, value='peca', inplace=True)
-    items_df.replace(saco, value='saco', inplace=True)
-    items_df.replace(caixa, value='caixa', inplace=True)
-    items_df.replace(comprimido, value='comprimido', inplace=True)
-    items_df.replace(pacote, value='pacote', inplace=True)
-    items_df.replace(kg, value='kg', inplace=True)
-    items_df.replace(frasco, value='frasco', inplace=True)
-    items_df.replace(litro, value='litro', inplace=True)
+    # for canonic, unit_list in unit_metric_canonical.items():
+        # items_df.replace(unit_list, value=canonic, inplace=True)
+
+    canonical_unit_metric = {}
+
+    for canonic, unit_list in unit_metric_canonical.items():
+        for unit in unit_list:
+            canonical_unit_metric[unit] = canonic
+
+    preprocessing = PreprocessingText(spellcheck=False)
+    stopwords = preprocessing.stopwords
+
+    items_df['dsc_unidade_medida'] = items_df['dsc_unidade_medida'].apply(lambda x: process_un_medida(x, stopwords, preprocessing, canonical_unit_metric))
