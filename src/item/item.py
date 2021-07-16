e76ed7f0241a8f1ced771802fdf0d454954b9ae7
pedrobrum <pedrobrum@dcc.ufmg.br>
2021-07-16 08:35:50 -0300

    Updated word dictionary

--- src/item/item.py
+++ src/item/item.py
@@ -93,6 +93,8 @@ class Item:
                 self.words.append(self.colors[0])
             elif len(self.sizes) > 0 and self.sizes[0] in set_ambiguous:
                 self.words.append(self.sizes[0])
+            elif len(self.unit_metrics) > 0 and self.unit_metrics[0] in set_ambiguous:
+                self.words.append(self.unit_metrics[0])
 
 
     def load_item(self, item, original):
